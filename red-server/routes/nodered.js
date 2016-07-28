6d470b2ba2339ea8b1fb085cfb3d1ea6b889bb10
tlodge <tlodge@gmail.com>
2016-07-28 15:31:15 +0100

    more on dockerising separate node-red instances

--- red-server/routes/nodered.js
+++ red-server/routes/nodered.js
@@ -5,33 +5,15 @@ import docker from '../utils/docker.js';
 
 const router = express.Router();
 
-router.post('/flows', function(req, res){
-	
-	console.log(req.user);
-	
-	docker.createContainer({Image: 'docker_red', Cmd: ['node', '/root/node-red/red.js'], name: `${req.user.username}-red`}, function (err, container) {
-  		if (err){
-  			console.log(err);
-  		}else{
-  			container.start({"PortBindings": { "1880/tcp": [{ "HostPort": "11022" }]}}, function (err, data) {
-  				if (err){
-  					console.log("error!");
-  					console.log(err);
-  				}else{
-  					console.log(data);
-  				}
-  			});
-  		}
-	});
-	
-	var data = req.body;
+const _postFlows = function(port, data, res){
+	console.log(`connecting to localhost:${port}/flows`);
 	
 	request
-   			.post(`${config.red.URL}/flows`)
+   			.post(`localhost:${port}/flows`)
    			.send(data)
    			.set('Accept', 'application/json')
    			.type('json')
-   			.end((err, data)=>{
+   			.end((err, result)=>{
      			
      			if (err) {
        				console.log(err);
@@ -41,7 +23,82 @@ router.post('/flows', function(req, res){
        				res.send({success:true});
      			}
    			});
+}
+
+const _waitForStart = function(container){
+	return new Promise((resolve,reject)=>{
+		container.attach({stream: true, stdout: true, stderr: true}, function (err, stream) {
+    		stream.on('data', function(line) {
+    			if (line.toString().indexOf("Started flows") != -1){
+    				resolve();	
+    			}
+    		});
+		});
+	});
+}
+
+
+const _startContainer = function(container, flows, res){
+	_waitForStart(container).then(function(){
+		container.inspect(function (err, cdata) {
+			console.log("starting container!");
+			let port = cdata['NetworkSettings']['Ports']['1880/tcp'][0]['HostPort'];
+			_postFlows(port, flows, res);
+		});
+	});
+}
+
+router.post('/flows', function(req, res){
 	
+	const flows = req.body;
+	
+	const opts = {
+		filters : {
+						label: [`user=${req.user.username}`],
+						status: ['running', "exited"],			
+		}
+  	}	
+	
+	docker.listContainers(opts, function(err, containers) {
+		console.log(`Containers labeled user=${req.user.username} ${containers.length}`);
+		
+		//create a new container and start it, if it doesn't exist
+		if (containers.length <= 0){
+			docker.createContainer({Image: 'docker_red', Labels: {'user':`${req.user.username}`}, Cmd: ['node', '/root/node-red/red.js'], name: `${req.user.username}-red`}, function (err, container) {
+				if (err){
+					console.log(err);
+				}else{
+					//{"PortBindings": { "1880/tcp": [{ "HostPort": "11022" }]}}
+					container.start({"PublishAllPorts":true}, function (err, data) {
+						if (err){
+							console.log("error!");
+							console.log(err);
+						}else{
+							_startContainer(container, flows, res);
+						}
+					});
+				}
+			});
+		}else{
+			const c = containers[0];
+			//restart the container if it exists but is stopped
+			if (c.State === 'exited'){
+				console.log("restarting container!!");
+				const container = docker.getContainer(c.Id);
+				container.restart({}, function(err, data){
+					if (err){
+						console.log(err);
+					}else{
+						_startContainer(container, flows, res);
+					}		
+				});
+			}else{
+				//post flows to already running container
+				let port = c.Ports[0]['PublicPort'];
+				_postFlows(port, flows, res);
+			}
+		}
+	});
 });
 
 module.exports = router;
