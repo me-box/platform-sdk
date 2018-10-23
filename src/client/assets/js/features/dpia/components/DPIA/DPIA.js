cb9d5581fca29e762d9145b7935931fd6dbd7c2c
tlodge <tlodge@gmail.com>
2018-10-23 14:09:22 +0100

    completed skeleton DPIA

--- src/client/assets/js/features/dpia/components/DPIA/DPIA.js
+++ src/client/assets/js/features/dpia/components/DPIA/DPIA.js
@@ -19,15 +19,50 @@ export default class DPIA extends Component {
     }
 
     renderDPIA() {
-        const { categories } = this.props;
+        const { categories, checked, status } = this.props;
 
-        console.log("DPIA CATEGORIES", categories);
 
         const dialogueprops = {
-            ok: (e) => { console.log("ok clikced"); this.props.actions.toggleDPIA() },
-            cancel: (e) => { console.log("cancel clikced"); this.props.actions.toggleDPIA() },
+            ok: (e) => { this.props.actions.toggleDPIA() },
+            cancel: (e) => { this.props.actions.toggleDPIA() },
+            close: (e) => { this.props.actions.toggleDPIA() },
             title: "DPIA",
         }
+
+        const items = categories.map((c) => {
+
+
+
+            const criteria = c.values.map(criterium => {
+                const selected = checked.indexOf(criterium.id) != -1;
+                return (<div className="criteria">
+                    <div className="text">
+                        <div>
+                            <div className="title">
+                                {criterium.title}
+                            </div>
+                            <div className="description">
+                                <div dangerouslySetInnerHTML={{ __html: criterium.description }} />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="check" onClick={(e) => this.props.actions.toggleChecked(criterium.id)}>
+                        {selected ? "yes" : "no"}
+                    </div>
+                </div>)
+            })
+
+            return <div className="category">
+                <div className="title">
+                    {c.title}
+                </div>
+                <div className="description">
+                    {c.description}
+                </div>
+                {criteria}
+            </div>
+        });
+
         return <div id="dpia">
             <Dialogue {...dialogueprops}>
                 <div className="content">
@@ -43,149 +78,18 @@ export default class DPIA extends Component {
                         </div>
                         <div className="status">
                             <div className="statusbutton">
-                                status: DPIA recommended
-                        </div>
-                        </div>
-                    </div>
-                    <div className="category">
-                        <div className="title">
-                            Nature of your data
-                    </div>
-                        <div className="description">
-                            The type of data being collected will strongly influence the likelihood and severity of risk your app exposes a user to
-                    </div>
-                        <div className="criteria">
-                            <div className="text">
-                                <div>
-                                    <div className="title">
-                                        Sensitive or personal  data use
-                                </div>
-                                    <div className="description">
-                                        Your app uses [] datasources which are deemed personal and [] which are deemed sensitive.   [] is exported off the box.   Were this data exposed to third parities (either intentionally or accidentally), would it result in any serious impact on the app user's daily life or lead to any loss of rights or freedoms?
-                                </div>
-                                </div>
+                                status: {status}
                             </div>
-                            <div className="check">
-                                yes
-                        </div>
-                        </div>
-                        <div className="criteria">
-                            <div className="text">
-                                <div>
-                                    <div className="title">
-                                        Evaluation / scoring
-                                </div>
-                                    <div className="description">
-                                        In the following nodes, personal data is being processed to generate an output. Please indicate whether any of the outputs below are used to evaluate or score, profile or predict the nature or behaviour of an app user.
-                                </div>
-                                </div>
-                            </div>
-                            <div className="check">
-                                yes
-                        </div>
                         </div>
                     </div>
-
-                    <div className="category">
-                        <div className="title">
-                            Data usage "off the box"
-                    </div>
-                        <div className="description">
-                            Your app is exporting data off the databox, which means that the user will relinquish control of this data.  There is an assumed elevated risk if it is used for any of the following:
-                    </div>
-                        <div className="criteria">
-                            <div className="text">
-                                <div>
-                                    <div className="title">
-                                        Automated decisions
-                                </div>
-                                    <div className="description">
-                                        Will any decisions be made, without human intervention, that may significantly affect the user (for example, legal decisions or access to services or resources, credit checks, loan applications, use of machine learning?
-                                </div>
-                                </div>
-                            </div>
-                            <div className="check">
-                                yes
-                        </div>
-                        </div>
-                        <div className="criteria">
-                            <div className="text">
-                                <div>
-                                    <div className="title">
-                                        Systematic monitoring / tracking
-                                </div>
-                                    <div className="description">
-                                        Will the data be used to monitor or track any aspect of a user or their behaviour (e.g. health data, location, occupancy, resource consumption)?
-                                </div>
-                                </div>
-                            </div>
-                            <div className="check">
-                                yes
-                        </div>
-                        </div>
-                        <div className="criteria">
-                            <div className="text">
-                                <div>
-                                    <div className="title">
-                                        Restriction of rights or access to a service / contract
-                                </div>
-                                    <div className="description">
-                                        Will the data be used to prevent or reduce access to a service or contract, or impinge upon a user's rights (e.g. freedom of speech, freedom of thought, freedom of movement, prohibition of discrimination, right to liberty, conscience and religion)?
-                                </div>
-                                </div>
-                            </div>
-                            <div className="check">
-                                yes
-                        </div>
-                        </div>
-                        <div className="criteria">
-                            <div className="text">
-                                <div>
-                                    <div className="title">
-                                        Matching or combining datasets
-                                </div>
-                                    <div className="description">
-                                        Will the data be combined with data from other data processing operations, performed for different purposes and/or by different data controllers in a way that would exceed the reasonable expectations of the data subject?
-                                </div>
-                                </div>
-                            </div>
-                            <div className="check">
-                                yes
-                        </div>
-                        </div>
-                    </div>
-
-
-                    <div className="category">
-                        <div className="title">
-                            Intended user
-                    </div>
-                        <div className="description">
-                            Certain groups of user are deemed more vulnerable to risk, and will require an enhanced risk assessment
-                    </div>
-                        <div className="criteria">
-                            <div className="text">
-                                <div>
-                                    <div className="title">
-                                        Typical user
-                                </div>
-                                    <div className="description">
-                                        Is your app aimed at children or vulnerable groups (e.g. the elderly, disabled, persecuted, health impaired)?
-                                </div>
-                                </div>
-                            </div>
-                            <div className="check">
-                                yes
-                        </div>
-                        </div>
-                    </div>
+                    {items}
                 </div>
             </Dialogue>
         </div>
     }
+
     render() {
         const { show } = this.props;
-
         return <div>
             {show && this.renderDPIA()}
         </div>
--- src/client/assets/js/features/dpia/dpia.js
+++ src/client/assets/js/features/dpia/dpia.js
@@ -3,10 +3,12 @@ import { createStructuredSelector, createSelector } from 'reselect';
 export const NAME = 'dpia';
 
 const TOGGLE_DPIA = 'iot.red/dpia/TOGGLE_DPIA';
+const TOGGLE_CHECKED = 'iot.red/dpia/TOGGLE_CHECKED';
 
 const initialState = {
     show: false,
     nid: null,
+    checked: [],
 }
 
 export default function reducer(state = initialState, action) {
@@ -19,6 +21,11 @@ export default function reducer(state = initialState, action) {
                 nid: state.show ? null : action.nid,
             }
 
+        case TOGGLE_CHECKED:
+            return {
+                ...state,
+                checked: action.checked,
+            }
         default:
             return state
 
@@ -32,8 +39,16 @@ function toggleDPIA(nid = null) {
     }
 }
 
-function toggleChecked(category, criteria) {
+function toggleChecked(criteria) {
+    return (dispatch, getState) => {
+        const currentlychecked = getState()[NAME].checked;
+        const checked = currentlychecked.indexOf(criteria) == -1 ? [...currentlychecked, criteria] : currentlychecked.filter(c => c != criteria);
 
+        dispatch({
+            type: TOGGLE_CHECKED,
+            checked,
+        });
+    }
 }
 
 const show = (state) => state[NAME].show;
@@ -58,6 +73,17 @@ const _inputs = (nid, links) => {
     }, [])
 }
 
+const _outputs = (nid, links) => {
+    return Object.keys(links).reduce((acc, key) => {
+        const link = links[key];
+
+        if (link && link.source && link.source.id === nid) {
+            acc = [...acc, link.target.id];
+        }
+        return acc;
+    }, [])
+}
+
 const upstream = (nid, links = {}) => {
 
     const parents = _inputs(nid, links);
@@ -70,6 +96,78 @@ const upstream = (nid, links = {}) => {
     }, []));
 }
 
+
+const path = (x1, x2, y1, y2) => {
+    return `M ${x1} ${y1} C ${x1 + (20)} ${y1} ${x2 - 20} ${y2} ${x2} ${y2}`
+}
+const renderNode = (node, links, nodes) => {
+
+    const inputs = _inputs(node, links).map(nid => {
+        return nodes[nid].name || nodes[nid].label;
+    });
+
+    if (inputs.length == 0) {
+        return null;
+    }
+
+    const outputs = _outputs(node, links).map(nid => {
+        return nodes[nid].name || nodes[nid].label;
+    })
+
+    const iorigin = inputs.length <= 1 ? 20 : 0;
+    const idelta = inputs.length <= 1 ? 0 : 40 / (inputs.length - 1);
+
+    const inputlines = inputs.map((input, idx) => {
+        return `<g>
+                    <path d="${path(50, 80, iorigin + (idx * idelta), 20)}" style="stroke:#465662; stroke-width:2px; fill:none;"/>
+                    <text style="font-size:0.8em; text-anchor:end; alignment-baseline:central" x=48 y=${iorigin + (idx * idelta)}>${input}</text>
+                </g>`
+    })
+
+    const oorigin = outputs.length <= 1 ? 20 : 0;
+    const odelta = outputs.length <= 1 ? 0 : 40 / (outputs.length - 1);
+
+    const outputlines = outputs.map((output, idx) => {
+        return `<g>
+                    <path d="${path(120, 150, 20, oorigin + (idx * odelta))}" style="stroke:#465662; stroke-width:2px; fill:none;"/>
+                    <text style="font-size:0.8em; text-anchor:start; alignment-baseline:central" x=150 y=${oorigin + (idx * odelta)}>${output}</text>
+                </g>`
+    })
+
+    const icontxt = nodes[node]._def.unicode || '\uf040'
+
+    return `<svg height="60" width="230">
+                    <g transform="translate(20,10)">
+                        ${inputlines}
+                        <rect x=80 y=0 height=40 width=40 style="stroke:#EBEBEB; stroke-width:2px; fill:${nodes[node]._def.color};"/>
+                        <text class="fa icon" x=100 y=20 style="text-anchor:middle;alignment-baseline:central">${icontxt}</text>
+                        <circle cx=80 cy=20 r=5 style="stroke:#465662; stroke-width:2px; fill:white;"/>
+                        ${outputlines}
+                        <circle cx=120 cy=20 r=5 style="stroke:#465662; stroke-width:2px; fill:white;"/>
+                    </g>
+                
+            </svg >`
+}
+const evaluationNodes = (upstream, links, nodes) => {
+
+    const overview = upstream.filter(id => ["profilers", "processors"].indexOf(nodes[id]._def.category) != -1).reduce((acc, n) => {
+        const rendered = renderNode(n, links, nodes);
+        if (rendered) {
+            acc = [...acc, `<div>${rendered}</div>`]
+        }
+        return acc;
+    }, []);
+
+
+    return `<div class="evaluation">
+                <p>In the following nodes, <strong>personal data is being processed</strong> to generate an output. Please indicate whether any of the outputs below are used to evaluate or score, profile or predict the nature or behaviour of an app user."
+                </p>
+                ${overview.join("")}
+            </div>`
+}
+
+
+
 const categories = createSelector([nid, links, nodes], (nid, links, nodes) => {
     if (nid) {
         const node = nodes[nid];
@@ -77,7 +175,6 @@ const categories = createSelector([nid, links, nodes], (nid, links, nodes) => {
 
         const datastores = Object.keys(nodes).reduce((acc, key) => {
             const n = nodes[key];
-            console.log("n is", n);
 
             if (n._def && n._def.category === "datastores") {
                 acc = [...acc, n];
@@ -86,7 +183,7 @@ const categories = createSelector([nid, links, nodes], (nid, links, nodes) => {
         }, []);
 
         const personaldata = datastores.reduce((acc, n) => {
-            console.log("n is", n);
+
             if (n._def && n._def.category === "datastores") {
                 if (n.schema && n.schema.output && n.schema.output.ptype) {
                     const ptype = n.schema.output.ptype;
@@ -118,23 +215,62 @@ const categories = createSelector([nid, links, nodes], (nid, links, nodes) => {
             return acc;
         }, []);
 
-        const personaldescription = `Your app uses ${datastores.length} data sources which emit ${pdatastr.join(" and ")} `;
-        console.log(personaldescription);
-
-
-        console.log("personal data is ", personaldata);
-
+        const personaldescription = `<p>Your app uses <strong> ${datastores.length} </strong> data sources which emit <strong> ${pdatastr.join(" and ")}</strong>. Were this data exposed to third parties (either intentionally or accidentally), would it result in any serious impact on the app user's daily life or lead to any loss of rights or freedoms?   </p> `;
         const upstreamnodes = upstream(node.id, links);
-        const nature = Object.keys(datastores).length > 0 ? [{ id: "personal", description: "" }, { id: "evaluation" }] : [];
-        const exported = [{ id: "automated" }, { id: "systematic" }, { id: "restriction" }, { id: "matching" }];
-        const users = [{ id: "vulnerable" }];
+        const enodes = evaluationNodes(upstreamnodes, links, nodes);
+
+        const nature = Object.keys(datastores).length > 0 ? [
+            { id: "personal", title: "Sensitive or personal data use", description: personaldescription },
+            { id: "evaluation", title: "Evaluation/scoring", description: enodes },
+
+        ] : [];
+
+        const exported = [
+            { id: "automated", title: "Automated decisions", description: "<p>Will any decisions be made, without human intervention, that may significantly affect the user (for example, legal decisions or access to services or resources, credit checks, loan applications, use of machine learning?</p>" },
+            { id: "systematic", title: "Systematic monitoring/tracking", description: "<p>Will the data be used to monitor or track any aspect of a user or their behaviour (e.g. health data, location, occupancy, resource consumption)?</p>" },
+            { id: "restriction", title: "Restriction of rights or access to a service / contract", description: "<p>Will the data be used to prevent or reduce access to a service or contract, or impinge upon a user's rights (e.g. freedom of speech, freedom of thought, freedom of movement, prohibition of discrimination, right to liberty, conscience and religion)?</p>" },
+            { id: "matching", title: "Matching or combining datasets", description: "<p>Will the data be combined with data from other data processing operations, performed for different purposes and/or by different data controllers in a way that would exceed the reasonable expectations of the data subject?</p>" }
+        ];
+
+        const users = [
+            { id: "vulnerable", title: "Typical user", description: "<p>Is your app aimed at children or vulnerable groups (e.g. the elderly, disabled, persecuted, health impaired)?" },
+        ];
+
+        return [
+            {
+                title: "nature of your data",
+                description: "The type of data being collected will strongly influence the likelihood and severity of risk your app exposes a user to",
+                values: nature,
+            },
+            {
+                title: "data exported off the databox",
+                description: "Your app is exporting data off the databox, which means that the user will relinquish control of this data.  There is an assumed elevated risk if it is used for any of the following:",
+                values: exported,
+            },
+            {
+                title: "intended user(s)",
+                description: "Certain groups of user are deemed more vulnerable to risk, and will require an enhanced risk assessment",
+                values: users,
+            }
+        ]
     }
-    return {}
+});
+
+const checked = (state) => state[NAME].checked;
+
+const status = createSelector([checked], (checked) => {
+    if (checked.length >= 2) {
+        return "DPIA recommended";
+    } else {
+        return "DPIA not required";
+    }
 });
 
 export const selector = createStructuredSelector({
     show,
     categories,
+    checked,
+    status,
 });
 
 export const actionCreators = {
